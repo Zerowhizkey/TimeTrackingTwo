a726250a70e1699265cf28da6fce278d9da00c15
Zerowhizkey <zerogranit@hotmail.se>
2022-11-04 08:56:27 +0100

    working on nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Box } from "@chakra-ui/react";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	Box,
+} from "@chakra-ui/react";
 const Navbar = () => {
 	return (
-		<Box left="0" bottom="0" position="fixed">
-			<Breadcrumb>
+		<Box left="0" bottom="0" position="fixed" w="100vw">
+			<Breadcrumb   display="flex" justifyContent="center"> 
 				<BreadcrumbItem>
 					<BreadcrumbLink as={Link} to="overview">
 						Overview
